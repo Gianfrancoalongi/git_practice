bdd2a087304da9be07f8bbd4eedabf3de3d8ce43
Gianfranco Alongi <gianfranco.alongi@gmail.com>
2013-12-20 12:51:54 +0100

    Added test for positive case when done with scenario_2

--- test/run_tests.bash
+++ test/run_tests.bash
@@ -9,6 +9,8 @@ rm -rf ${DIR} &> /dev/null
 bash ../scenario_2.bash &> /dev/null
 DIR=$(cat repository.txt)
 [[ $(bash ../scenario_2.bash --verify ${DIR}) == "No - you are not done" ]] && echo "T2_neg passed" || echo "T2_neg failed"
+echo '*.txt' > ${DIR}/.gitignore
+[[ $(bash ../scenario_2.bash --verify ${DIR}) == "Verified - you are done" ]] && echo "T2_pos passed" || echo "T2_pos failed"
 rm -rf ${DIR} &> /dev/null
 
 
