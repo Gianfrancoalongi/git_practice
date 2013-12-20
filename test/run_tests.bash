0624675117866c6762aa162b40168b79ea06d161
Gianfranco Alongi <gianfranco.alongi@gmail.com>
2013-12-20 12:55:16 +0100

    Refactoring tests

--- test/run_tests.bash
+++ test/run_tests.bash
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+DONE="Verified - you are done"
+NOT_DONE="No - you are not done"
+
 DIR=$(mktemp -d)
-[[ $(bash ../scenario_1.bash --verify ${DIR}) == "No - you are not done" ]] && echo "T1_neg passed" || echo "T1_neg failed"
+[[ $(bash ../scenario_1.bash --verify ${DIR}) == ${NOT_DONE} ]] && echo "T1_neg passed" || echo "T1_neg failed"
 git init ${DIR} &> /dev/null
-[[ $(bash ../scenario_1.bash --verify ${DIR}) == "Verified - you are done" ]] && echo "T1_pos passed" || echo "T1_pos failed"
+[[ $(bash ../scenario_1.bash --verify ${DIR}) == ${DONE} ]] && echo "T1_pos passed" || echo "T1_pos failed"
 rm -rf ${DIR} &> /dev/null
 
 bash ../scenario_2.bash &> /dev/null
 DIR=$(cat repository.txt)
-[[ $(bash ../scenario_2.bash --verify ${DIR}) == "No - you are not done" ]] && echo "T2_neg passed" || echo "T2_neg failed"
+[[ $(bash ../scenario_2.bash --verify ${DIR}) == ${NOT_DONE} ]] && echo "T2_neg passed" || echo "T2_neg failed"
 echo '*.txt' > ${DIR}/.gitignore
-[[ $(bash ../scenario_2.bash --verify ${DIR}) == "Verified - you are done" ]] && echo "T2_pos passed" || echo "T2_pos failed"
+[[ $(bash ../scenario_2.bash --verify ${DIR}) == ${DONE} ]] && echo "T2_pos passed" || echo "T2_pos failed"
 rm -rf ${DIR} &> /dev/null
-
 
 \rm *.txt
