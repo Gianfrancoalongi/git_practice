1ae8f3e5027e4ffbe964b06128a9db2ce215a19b
Gianfranco Alongi <gianfranco.alongi@gmail.com>
2013-12-20 13:03:17 +0100

    Refactoring the tests - extract common method for pos verification

--- test/run_tests.bash
+++ test/run_tests.bash
@@ -8,6 +8,23 @@ main() {
     scenario_2_tests
 }
 
+scenario_1_tests() {
+    DIR=$(mktemp -d)
+    test_that_verification_fails_for_scenario 1
+    git init ${DIR} &> /dev/null
+    test_that_verification_passes_for_scenario 1
+    rm -rf ${DIR} &> /dev/null
+}
+
+scenario_2_tests() {
+    bash ../scenario_2.bash &> /dev/null
+    DIR=$(cat repository.txt)
+    test_that_verification_fails_for_scenario 2
+    echo '*.txt' > ${DIR}/.gitignore
+    test_that_verification_passes_for_scenario 2
+    rm -rf ${DIR} &> /dev/null
+}
+
 test_that_verification_fails_for_scenario() {
    if [[ $(bash ../scenario_${1}.bash --verify ${DIR}) == ${NOT_DONE} ]] 
     then
@@ -17,31 +34,13 @@ test_that_verification_fails_for_scenario() {
     fi
 }
 
-scenario_1_tests() {
-    DIR=$(mktemp -d)
-    test_that_verification_fails_for_scenario 1
-    git init ${DIR} &> /dev/null
-    if [[ $(bash ../scenario_1.bash --verify ${DIR}) == ${DONE} ]]
+test_that_verification_passes_for_scenario() {
+    if [[ $(bash ../scenario_${1}.bash --verify ${DIR}) == ${DONE} ]]
     then
-	echo "T1_pos passed" 
+	echo "T${1}_pos passed" 
     else 
-	echo "T1_pos failed"
+	echo "T${1}_pos failed"
     fi
-    rm -rf ${DIR} &> /dev/null
-}
-
-scenario_2_tests() {
-    bash ../scenario_2.bash &> /dev/null
-    DIR=$(cat repository.txt)
-    test_that_verification_fails_for_scenario 2
-    echo '*.txt' > ${DIR}/.gitignore
-    if [[ $(bash ../scenario_2.bash --verify ${DIR}) == ${DONE} ]] 
-    then
-	echo "T2_pos passed" 
-    else
-	echo "T2_pos failed"
-    fi
-    rm -rf ${DIR} &> /dev/null
 }
 
 main
