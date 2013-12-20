22bc572feb4d30545262f14eb1866ac3da0e1890
Gianfranco Alongi <gianfranco.alongi@gmail.com>
2013-12-20 12:57:30 +0100

    Refactoring the tests some more

--- test/run_tests.bash
+++ test/run_tests.bash
@@ -3,17 +3,38 @@
 DONE="Verified - you are done"
 NOT_DONE="No - you are not done"
 
-DIR=$(mktemp -d)
-[[ $(bash ../scenario_1.bash --verify ${DIR}) == ${NOT_DONE} ]] && echo "T1_neg passed" || echo "T1_neg failed"
-git init ${DIR} &> /dev/null
-[[ $(bash ../scenario_1.bash --verify ${DIR}) == ${DONE} ]] && echo "T1_pos passed" || echo "T1_pos failed"
-rm -rf ${DIR} &> /dev/null
+main() {
+    scenario_1_tests
+    scenario_2_tests
+}
 
-bash ../scenario_2.bash &> /dev/null
-DIR=$(cat repository.txt)
-[[ $(bash ../scenario_2.bash --verify ${DIR}) == ${NOT_DONE} ]] && echo "T2_neg passed" || echo "T2_neg failed"
-echo '*.txt' > ${DIR}/.gitignore
-[[ $(bash ../scenario_2.bash --verify ${DIR}) == ${DONE} ]] && echo "T2_pos passed" || echo "T2_pos failed"
-rm -rf ${DIR} &> /dev/null
+scenario_1_tests() {
+    DIR=$(mktemp -d)
+    if [[ $(bash ../scenario_1.bash --verify ${DIR}) == ${NOT_DONE} ]] 
+    then
+	echo "T1_neg passed"
+    else 
+	echo "T1_neg failed"
+    fi
+    git init ${DIR} &> /dev/null
+    if [[ $(bash ../scenario_1.bash --verify ${DIR}) == ${DONE} ]]
+    then
+	echo "T1_pos passed" 
+    else 
+	echo "T1_pos failed"
+    fi
+    rm -rf ${DIR} &> /dev/null
+}
+
+scenario_2_tests() {
+    bash ../scenario_2.bash &> /dev/null
+    DIR=$(cat repository.txt)
+    [[ $(bash ../scenario_2.bash --verify ${DIR}) == ${NOT_DONE} ]] && echo "T2_neg passed" || echo "T2_neg failed"
+    echo '*.txt' > ${DIR}/.gitignore
+    [[ $(bash ../scenario_2.bash --verify ${DIR}) == ${DONE} ]] && echo "T2_pos passed" || echo "T2_pos failed"
+    rm -rf ${DIR} &> /dev/null
+}
+
+main
 
 \rm *.txt
