af8e8294ff7beeb07af2a6f99e2913872e99b65c
Gianfranco Alongi <gianfranco.alongi@gmail.com>
2013-12-20 13:10:31 +0100

    Refactoring - passing the dir and not using global DIR variable

--- test/run_tests.bash
+++ test/run_tests.bash
@@ -10,32 +10,32 @@ main() {
 
 scenario_1_tests() {
     DIR=$(mktemp -d)
-    test_that_verification_fails_for_scenario 1
+    test_that_verification_fails_for_scenario 1 ${DIR}
     git init ${DIR} &> /dev/null
-    test_that_verification_passes_for_scenario 1
+    test_that_verification_passes_for_scenario 1 ${DIR}
     rm -rf ${DIR} &> /dev/null
 }
 
 scenario_2_tests() {
     bash ../scenario_2.bash &> /dev/null
     DIR=$(cat repository.txt)
-    test_that_verification_fails_for_scenario 2
+    test_that_verification_fails_for_scenario 2 ${DIR}
     echo '*.txt' > ${DIR}/.gitignore
-    test_that_verification_passes_for_scenario 2
+    test_that_verification_passes_for_scenario 2 ${DIR}
     rm -rf ${DIR} &> /dev/null
 }
 
 test_that_verification_fails_for_scenario() {
-   if [[ $(bash ../scenario_${1}.bash --verify ${DIR}) == ${NOT_DONE} ]] 
+    if [[ $(bash ../scenario_${1}.bash --verify ${2}) == ${NOT_DONE} ]] 
     then
-	echo "T${1}_neg passed"
+    	echo "T${1}_neg passed"
     else 
-	echo "T${1}_neg failed"
+    	echo "T${1}_neg failed"
     fi
 }
 
 test_that_verification_passes_for_scenario() {
-    if [[ $(bash ../scenario_${1}.bash --verify ${DIR}) == ${DONE} ]]
+    if [[ $(bash ../scenario_${1}.bash --verify ${2}) == ${DONE} ]]
     then
 	echo "T${1}_pos passed" 
     else 
