1af83a2045f69a8b7387325c93811245436d40b2
Gianfranco Alongi <gianfranco.alongi@gmail.com>
2013-12-20 08:24:05 +0100

    Basic skeleton

---
+++ scenario_1.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat > description.txt <<EOF
+This is the most basic scenario - you will set up an empty git repository.
+EOF
+
+cat > help.txt <<EOF
+You can read Chapter 2.1 Git Basics - Getting a Git Repository for help
+EOF
+
+
+
