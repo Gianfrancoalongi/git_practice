23204304a5edcb3fef6547b14ef61f9d05f33953
gianfranco <gianfranco.alongi@gmail.com>
2014-01-03 15:35:25 +0100

    Modified solution functions to have same numbering scheme as the scenarios

--- test/run_tests.bash
+++ test/run_tests.bash
@@ -7,31 +7,26 @@ main() {
 
    scenario_1_tests
 
-   for((x=2;x<=9;x++))
+   for((x=2;x<=13;x++))
    do
-       bash ../scenario_0${x}.bash &> /dev/null
-       DIR=$(cat repository.txt)
-       test_that_verification_fails_for_scenario 0${x} ${DIR}
-       pushd ${DIR} &> /dev/null
-       solution_for_scenario_${x} ${DIR}
-       popd &> /dev/null
-       test_that_verification_passes_for_scenario 0${x} ${DIR}
-       rm -rf ${DIR} &> /dev/null
-   done
-   for((x=10;x<=13;x++))
-   do
-       bash ../scenario_${x}.bash &> /dev/null
+       if [[ ${x} -lt 10 ]]
+       then
+	   NUM=0${x}
+       else
+	   NUM=${x}
+       fi
+       bash ../scenario_${NUM}.bash &> /dev/null
        DIR=$(cat repository.txt)
        if [[ ${x} -gt 12 ]]
        then
 	   REMOTE=$(cat remote_repository.txt)
        fi
-       test_that_verification_fails_for_scenario ${x} ${DIR}
+       test_that_verification_fails_for_scenario ${NUM} ${DIR}
        pushd ${DIR} &> /dev/null
-       solution_for_scenario_${x} ${DIR}
+       solution_for_scenario_${NUM} ${DIR}
        popd &> /dev/null
-       test_that_verification_passes_for_scenario ${x} ${DIR}
-       rm -rf ${DIR} &> /dev/null
+       test_that_verification_passes_for_scenario ${NUM} ${DIR}
+       rm -rf ${DIR} &> /dev/null       
    done
 }
 
@@ -43,41 +38,41 @@ scenario_1_tests() {
     rm -rf ${DIR} &> /dev/null
 }
 
-solution_for_scenario_2() {
+solution_for_scenario_02() {
     echo '*.txt' > ${1}/.gitignore
 }
 
-solution_for_scenario_3() {
+solution_for_scenario_03() {
     git config --local core.editor 'emacs -nw'
     git config --local merge.tool kdiff3
 }
 
-solution_for_scenario_4() {
+solution_for_scenario_04() {
     echo 'I made this.' >> file.txt
     git add file.txt &> /dev/null
     git commit -m "Added the file as requested." &> /dev/null
 }
 
-solution_for_scenario_5() {
+solution_for_scenario_05() {
     git reset HEAD b.txt &> /dev/null
 }
 
-solution_for_scenario_6() {
+solution_for_scenario_06() {
     git commit --amend -m 'Correct commit message.' &> /dev/null
 }
 
-solution_for_scenario_7() {
+solution_for_scenario_07() {
     git checkout a.txt
 }
 
-solution_for_scenario_8() {
+solution_for_scenario_08() {
     touch a.txt
     git add a.txt
     git commit -m 'Initial commit' &> /dev/null
     git checkout -b my_branch &> /dev/null
 }
 
-solution_for_scenario_9() {
+solution_for_scenario_09() {
     git checkout master &> /dev/null
     git merge ahead_of_master &> /dev/null
     git branch -D ahead_of_master &> /dev/null
