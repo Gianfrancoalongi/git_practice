0a63be25cabc81563d609deb73e7b987843cd7e5
gianfranco <gianfranco.alongi@gmail.com>
2014-01-17 07:56:54 +0100

    Changed layout to new and better layout. Also aligned with book and naming kdiff3 as the merge tool instead of diff tool.

--- scenario_03.bash
+++ scenario_03.bash
@@ -14,24 +14,28 @@ main() {
 
 show_scenario_text() {
     cat <<EOF
-*****************************************************************
-Scenario set up.
-You can always read 
-    description.txt to know what you need to do
-    help.txt to get pointers on what to read in order to succeed
-    repository.txt  to see where the scenario is created
-*****************************************************************
-Run this script as
-       $0 --verify ${SCENARIO_GIT_REPO}
-when you think you are done
-*****************************************************************
+=================================================================
+Your scenario GIT repository is in ${SCENARIO_GIT_REPO}
+=================================================================
 EOF
-echo "> description.txt"
 cat description.txt
-echo "*****************************************************************"
-echo "> help.txt"
+cat <<EOF
+=================================================================
+Recommended reading in Pro Git            http://git-scm.com/book
+EOF
 cat help.txt
-echo "*****************************************************************"
+cat <<EOF
+=================================================================
+Run this script as
+       bash $0 --verify ${SCENARIO_GIT_REPO}
+when you think you are done
+=================================================================
+You can always read 
+    description.txt To know what you need to do
+    help.txt        To get Pointers on what to read
+    repository.txt  To see where the scenario GIT repository is
+=================================================================
+EOF
 }
 
 setup_scenario() {
@@ -44,13 +48,10 @@ setup_scenario() {
 
 generate_description_file() {
     cat > description.txt <<EOF
-Configure the git repository locally so that the editor is configured 
-to emacs in no-window mode, that is
-     emacs -nw 
-and that the diff tool is configured to be kdiff3
-
-You can find the repository location in the file named 
-    repository.txt
+Configure the git repository locally so that 
+ (1) the editor is set to emacs in no-window mode, that is 
+     emacs -nw
+ (2) the merge tool is configured to kdiff3
 EOF
 }
 
