28dbd30aab165a4c1f4635b0742cc7a37e532cfa
gianfranco <gianfranco.alongi@gmail.com>
2014-01-17 08:03:07 +0100

    Improving the text and layout according to new format.

--- scenario_04.bash
+++ scenario_04.bash
@@ -14,24 +14,28 @@ main() {
 
 show_scenario_text() {
     cat <<EOF
-*****************************************************************
-Scenario set up.
-You can always read 
-    description.txt to know what you need to do
-    help.txt to get pointers on what to read in order to succeed
-    repository.txt  to see where the scenario is created
-*****************************************************************
-Run this script as
-       $0 --verify ${SCENARIO_GIT_REPO}
-when you think you are done
-*****************************************************************
+=================================================================
+Your scenario GIT repository is in ${SCENARIO_GIT_REPO}
+=================================================================
 EOF
-echo "> description.txt"
 cat description.txt
-echo "*****************************************************************"
-echo "> help.txt"
+cat <<EOF
+=================================================================
+Recommended reading in Pro Git            http://git-scm.com/book
+EOF
 cat help.txt
-echo "*****************************************************************"
+cat <<EOF
+=================================================================
+Run this script as
+       bash $0 --verify ${SCENARIO_GIT_REPO}
+when you think you are done
+=================================================================
+You can always read 
+    description.txt To know what you need to do
+    help.txt        To get Pointers on what to read
+    repository.txt  To see where the scenario GIT repository is
+=================================================================
+EOF
 }
 
 setup_scenario() {
@@ -44,16 +48,9 @@ setup_scenario() {
 
 generate_description_file() {
     cat > description.txt <<EOF
-Add a file called file.txt containing the text
-
-    I made this!
-
-to the git repository, commit it with the commit message
-
-    Added the file as requested.
-
-You can find the repository location in the file named 
-    repository.txt
+Add a file called file.txt containing the text 'I made this!'
+to the git repository, commit it with the exact commit message
+'Added the file as requested.'
 EOF
 }
 
