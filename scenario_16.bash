4dd8b9e8073d280865041a9d3e80636baad8e450
gianfranco <gianfranco.alongi@gmail.com>
2014-01-24 09:07:34 +0100

    Scenario setup is complete with new alternate method

--- scenario_16.bash
+++ scenario_16.bash
@@ -5,7 +5,7 @@ main() {
     then
 	check_that_cherry_was_picked ${2}
     else
-	setup_scenario &> /dev/null
+	setup_scenario
 	generate_description_file
 	generate_help_file
         bash user_text.bash $0
@@ -16,51 +16,70 @@ setup_scenario() {
     SCENARIO_GIT_REPO=$(mktemp -d)
     pushd ${SCENARIO_GIT_REPO}
     git init .
-    git config --local merge.tool kdiff3 
-    cat > story.txt <<EOF
-Once upon a time
-  there was an old man living at the bottom of a big big mountain.
-At the top of the big big mountain lived another old man.
-Both old men had been living in their huts for over 30 years and 
-frequently went out to collect wood and herbs. 
+    cat > training.txt <<EOF
+1.  run 2 kilometers
+2.
+3.
+4.
+5.  do 50 push ups
+6.
+7.
+8.
+9.  do 50 sit ups
+10.
+11.
+12.
+13. do 50 push ups
+14.
+15.
+16.
+17. do 50 sit ups
 EOF
-    git add story.txt
-    git commit -m 'first draft of story'
-    cat >> story.txt <<EOF
-One day the man at the top of the big big mountain dropped a piece
-of firewood as he was walking along the edge. And the firewood 
-dropped all the way, from the top of the big big mountain, down
-to the bottom of the big big mountain. 
-EOF
-    git commit -a -m 'the drop'
-    git checkout -b experimental
-    TMP=(mktemp)
-    sed 's/big big/big/g' story.txt > ${TMP} && mv ${TMP} story.txt
-    git commit -a -m 'removing duplicate bigs - makes story too wordy'
-    TMP=(mktemp)
-    sed 's/firewood/branch/g' story.txt > ${TMP} && mv ${TMP} story.txt
-    git commit -a -m 'the old men collected wood instead'
+    git add training.txt
+    git commit -m 'initial instructions'
+
+    # more cardio and less strength
+    git checkout -b more_cardio
+    TMP=$(mktemp)
+    sed 's/^3./3.  run 2 kilometers/g' training.txt > ${TMP} && mv ${TMP} training.txt
+    sed 's/^7./7.  run 2 kilometers/g' training.txt > ${TMP} && mv ${TMP} training.txt
+    sed 's/^11./11. run 2 kilometers/g' training.txt > ${TMP} && mv ${TMP} training.txt
+    sed 's/^15./15. run 2 kilometers/g' training.txt > ${TMP} && mv ${TMP} training.txt
+    git commit -a -m 'more running'
+    sed 's/50 push ups/10 push ups/g' training.txt > ${TMP} && mv ${TMP} training.txt
+    sed 's/50 sit ups/10 sit ups/g' training.txt > ${TMP} && mv ${TMP} training.txt
+    git commit -a -m 'less strength focus'
+    sed 's/^2./2.  10 min recovery/g'  training.txt > ${TMP} && mv ${TMP} training.txt
+    sed 's/^4./4.  10 min recovery/g'  training.txt > ${TMP} && mv ${TMP} training.txt
+    sed 's/^8./8.  10 min recovery/g'  training.txt > ${TMP} && mv ${TMP} training.txt
+    sed 's/^12./12. 10 min recovery/g'  training.txt > ${TMP} && mv ${TMP} training.txt
+    sed 's/^16./16. 10 min recovery/g'  training.txt > ${TMP} && mv ${TMP} training.txt
+    git commit -a -m '10 min recovery after running'
 
     git checkout master
-    cat >> story.txt <<EOF
-And so, a horse came walking down the mountain, and it came to be 
-that the firewood falling from the top, bouncing on all the rocks 
-and stones, on all the trees, and so finally hitting the door of 
-the old man at the bottom came to rest just as the horse passed
-the door. The old man opened the door and asked who came to visit
-him? 
-EOF
-    git commit -a -m 'the long face'
+    
+    # more strength and less cardio
+    git checkout -b more_strength
+    sed 's/50 push ups/100 push ups/g' training.txt > ${TMP} && mv ${TMP} training.txt
+    sed 's/50 sit ups/200 sit ups/g' training.txt > ${TMP} && mv ${TMP} training.txt
+    git commit -a -m 'more strength'
+    sed 's/^3./3.  20 squats with 60 kg backpack/g'  training.txt > ${TMP} && mv ${TMP} training.txt
+    sed 's/^7./7.  20 squats with 60 kg backpack/g'  training.txt > ${TMP} && mv ${TMP} training.txt
+    sed 's/^11./11. 20 squats with 60 kg backpack/g'  training.txt > ${TMP} && mv ${TMP} training.txt
+    sed 's/^15./15. 20 squats with 60 kg backpack/g'  training.txt > ${TMP} && mv ${TMP} training.txt
+    git commit -a -m 'more leg strength'
+
+    git checkout master
     popd
     echo ${SCENARIO_GIT_REPO} > repository.txt
 }
 
 generate_description_file() {
     cat > description.txt <<EOF
-Cherry pick the commit which removes duplicate bigs.
-This commit is on the experimental branch with the commit
-message 'removing duplicate bigs - make story too wordy'.
-You might have to do a manual merge conflict resolution.
+Cherry pick commits from the branches 'more_strength' and
+'more_cardio' so that the training program on the main
+branch contains 2 sets of 100 push ups, 2 sets of 200 sit 
+ups and 5 sets of 2 kilometer runs.
 EOF
 }
 
@@ -90,28 +109,13 @@ first draft of story
 EOF
     FACIT_STORY=$(mktemp)
 cat > ${FACIT_STORY} <<EOF    
-Once upon a time
-  there was an old man living at the bottom of a big mountain.
-At the top of the big mountain lived another old man.
-Both old men had been living in their huts for over 30 years and
-frequently went out to collect wood and herbs.
-One day the man at the top of the big mountain dropped a piece
-of firewood as he was walking along the edge. And the firewood
-dropped all the way, from the top of the big mountain, down
-to the bottom of the big mountain.
-And so, a horse came walking down the mountain, and it came to be
-that the firewood falling from the top, bouncing on all the rocks
-and stones, on all the trees, and so finally hitting the door of
-the old man at the bottom came to rest just as the horse passed
-the door. The old man opened the door and asked who came to visit
-him?
 EOF
     ACTUAL_BRANCH=$(mktemp)
     git branch &> ${ACTUAL_BRANCH}
     ACTUAL_LOG=$(mktemp)
     git log --format='%s' &> ${ACTUAL_LOG}
     ACTUAL_STORY=$(mktemp)
-    cat story.txt &> ${ACTUAL_STORY}
+    cat training.txt &> ${ACTUAL_STORY}
     
     diff -E -b ${FACIT_BRANCH} ${ACTUAL_BRANCH} &> /dev/null
     R1=$? 
